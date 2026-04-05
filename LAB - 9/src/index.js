f0283a7a48eb3a698ec89175500b8b8518a83a3c
vaibhavvanshu <145923488+vaibhavvanshu@users.noreply.github.com>
2026-04-05 12:15:07 +0530

    Refactor index.js to use App component with StrictMode

--- LAB - 9/src/index.js
+++ LAB - 9/src/index.js
@@ -1,6 +1,12 @@
+// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import StudentProfile from "./App";
+import App from "./App";
+import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<StudentProfile />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
